2ff797f0515842010d1a7d3042997e072d7a0ed7
ratanovalexandr <ratanovalexandr@gmail.com>
2022-07-13 19:18:08 +0300

    add1 src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  const paintCards = document.querySelectorAll('li');
+  paintCards.forEach(function (item, index) {
+    if (index % 2 === 0) item.style.backgroundColor = 'red';
+  });
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+  const findElement = document.getElementsByClassName('item likedItem');
+
+  findElement[0].style.background = 'blue';
+};
